28e15ed3fb7a8feb0b4ee51cbefbee3ecdbb7228
Ben <wellandbb@dulwich.org.uk>
2019-01-20 19:06:07 +0000

    Added tribe measure view
    created view to draw tribe
    implimented to all teams across tribe
    moved current measure to view variable
    implimented single responsibilty

--- Team management, Dad's project.py
+++ Team management, Dad's project.py
@@ -14,20 +14,16 @@ pygame.display.set_caption('Team Managment')
 clock = pygame.time.Clock()
 
 class team(object):
-    def __init__(self, points, y, x, colour, measures):
-        self.points = points
+    def __init__(self, y, x, colour, measures):
         self.y = y
         self.x = x
         self.colour = colour
         self.measures = measures
-        
-    def draw(self, win):
-        pygame.draw.rect(win, self.colour, (self.x, self.y, self.points, 40))
 
-    def getMeasure(self, measure):
-        for items in self.measures:
-            if items.getName == measure:
-                print(items.getValue)
+    def getMeasure(self, selectedMeasure):
+        for measure in self.measures:
+            if measure.getName() == selectedMeasure:
+                return measure.getValue()
                 
 
 class measure(object):
@@ -42,33 +38,49 @@ class measure(object):
         return self.value
 
 class tribe(object):
-    def __init__(self, teams, currentMeasure):
+    def __init__(self, teams):
         self.teams = teams
+
+
+    def getTeams(self):
+        return self.teams
+
+
+class tribeMeasureView(object):
+    def __init__(self, tribe, currentMeasure):
+        self.tribe = tribe
         self.currentMeasure = currentMeasure
 
     def changeMeasure(self, measure):
         self.currentMeasure = measure
+        print(self.currentMeasure)
 
-    def showMeasure(self):
-        for items in self.teams:
-            items.getMeasure(self.currentMeasure)
-        
-        
 
+    def draw(self, win):
+        for team in self.tribe.getTeams():
+            pygame.draw.rect(win, team.colour, (team.x, team.y, team.getMeasure(self.currentMeasure), 40))
+            
+    
+
+
+
+
+m1 = measure('releaseRate', 80)
+m2 = measure('operationalIncidents', 50)
+m3 = measure('releaseRate', 190)
+m4 = measure('operationalIncidents', 120)
+teamOne = team( 30, 10, red, [m1,m2])
+teamTwo = team(80, 10, blue,[m3,m4])
+teamThree = team(130, 10, green,[m3,m2])
+teamFour = team(180, 10, red,[m1,m4])
+teamFive = team(230, 10, blue,[m1,m2])
+teamSix = team(280, 10, green,[m3,m4])
+teamSeven = team(330, 10, red,[m3,m2])
+teamEight = team(380, 10, blue,[m1,m4])
+tribeOne = tribe([teamOne,teamTwo,teamThree,teamFour,teamFive,teamSix,teamSeven,teamEight])
+viewOne = tribeMeasureView(tribeOne, 'operationalIncidents')
 #mainloop
 run = True
-
-m1 = measure('releaseRate', 3)
-m2 = measure('operationalIncidents', 1)
-teamOne = team(20, 30, 10, red, [m1,m2])
-tribeOne = tribe([teamOne], 'releaseRate')
-#teamTwo = team(20, 80, 10, blue)
-#teamThree = team(20, 130, 10, green)
-#teamFour = team(20, 180, 10, red)
-#teamFive = team(20, 230, 10, blue)
-#teamSix = team(20, 280, 10, green)
-#teamSeven = team(20, 330, 10, red)
-#teamEight = team(20, 380, 10, blue)
 while run:
 
     clock.tick(27)
@@ -78,22 +90,13 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        tribeOne.showMeasure()
+    if keys[pygame.K_UP]:
+        viewOne.changeMeasure('operationalIncidents')
+    if keys[pygame.K_DOWN]:
+        viewOne.changeMeasure('releaseRate')
 
-
-
-    teamOne.draw(win)
-    #teamTwo.draw(win)
-    #teamThree.draw(win)
-    #teamFour.draw(win)
-    #teamFive.draw(win)
-    #teamSix.draw(win)
-    #teamSeven.draw(win)
-    #teamEight.draw(win)
-
-
-    
+    win.fill((0,0,0))
+    viewOne.draw(win)
     pygame.display.update()
     
 pygame.quit()
